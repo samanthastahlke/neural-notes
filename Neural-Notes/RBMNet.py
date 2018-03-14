13e3718f317407da8dbaf021c07ba98873aff7c3
samanthastahlke <samanthastahlke@gmail.com>
2018-03-14 19:05:24 -0400

    Intermediary commit, working on refactoring, structure, and prepping for enhancements/expansion.

--- Neural-Notes/RBMNet.py
+++ Neural-Notes/RBMNet.py
@@ -5,14 +5,61 @@ from tqdm import tqdm
 import numpy as num
 import os
 
+DEFAULT_TIMESTEPS = 15
+DEFAULT_HNODES = 50
+DEFAULT_EPOCHS = 200
+DEFAULT_BATCHSIZE = 100
+DEFAULT_LEARNRATE = 0.005
+
+
+def ProbSample(p):
+    return tf.floor(p + tf.random_uniform(tf.shape(p), 0, 1))
+
+
+def Gibbs(k, x, wMatrix, hBias, vBias):
+
+    def GibbsStep(count, k, xk):
+        hk = ProbSample(tf.sigmoid(tf.matmul(xk, wMatrix) + hBias))
+        xk = ProbSample(tf.sigmoid(tf.matmul(hk, tf.transpose(wMatrix)) + vBias))
+        return count + 1, k, xk
+
+    ct = tf.constant(0)
+    [_, _, x_sample] = control_flow_ops.while_loop(lambda count, num_iter, *args: count < num_iter, GibbsStep,
+                                                   [ct, tf.constant(k), x])
+
+    x_sample = tf.stop_gradient(x_sample)
+    return x_sample
+
 class RBMNet:
 
     def __init__(self, midiUtil, minTrainingSnippet=32):
         self.midi = midiUtil
         self.minTrainingSnippet = minTrainingSnippet
+
         self.trainDataset = []
+        self.trained = False
+
+        self.InitDefaultParameters()
+
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
         return
+
+    def InitDefaultParameters(self):
+        self.notespan = self.midi.notespan
+        self.timesteps = DEFAULT_TIMESTEPS
+
+        self.vNodes = 2 * self.notespan * self.timesteps
+        self.hNodes = DEFAULT_HNODES
+
+        self.epochs = DEFAULT_EPOCHS
+        self.batchSize = DEFAULT_BATCHSIZE
+        self.learnRate = tf.constant(DEFAULT_LEARNRATE, tf.float32)
+
+        self.notedata = tf.placeholder(tf.float32, [None, self.vNodes], name="notedata")
+        self.wMatrix = tf.Variable(tf.random_normal([self.vNodes, self.hNodes], 0.01), name="wMatrix")
+
+        self.vBias = tf.Variable(tf.zeros([1, self.vNodes], tf.float32, name="vBias"))
+        self.hBias = tf.Variable(tf.zeros([1, self.hNodes], tf.float32, name="hBias"))
 
     def LoadTrainingSet(self, directory):
 
@@ -44,80 +91,52 @@ class RBMNet:
         if not len(self.trainDataset) > 0:
             print("Can't train without any data!")
             return
+        
+        note_sample = Gibbs(k=1, x=self.notedata, wMatrix=self.wMatrix, hBias=self.hBias, vBias=self.vBias)
+        h = ProbSample(tf.sigmoid(tf.matmul(self.notedata, self.wMatrix) + self.hBias))
+        h_sample = ProbSample(tf.sigmoid(tf.matmul(note_sample, self.wMatrix) + self.hBias))
 
-        notespan = self.midi.notespan
+        size_bt = tf.cast(tf.shape(self.notedata)[0], tf.float32)
+        W_adder = tf.multiply(self.learnRate / size_bt,
+                              tf.subtract(tf.matmul(tf.transpose(self.notedata), h), tf.matmul(tf.transpose(note_sample), h_sample)))
 
-        timesteps = 15
-        vLayerNodes = 2 * notespan * timesteps
-        hLayerNodes = 50
-
-        epochCount = 200
-        batchSize = 100
-
-        learningRate = tf.constant(0.005, tf.float32)
-
-        x = tf.placeholder(tf.float32, [None, vLayerNodes], name="x")
-        weightMatrix = tf.Variable(tf.random_normal([vLayerNodes, hLayerNodes], 0.01), name="weightMatrix")
-
-        hBias = tf.Variable(tf.zeros([1, hLayerNodes], tf.float32, name="hBias"))
-        vBias = tf.Variable(tf.zeros([1, vLayerNodes], tf.float32, name="vBias"))
-
-        def ProbSample(p):
-            return tf.floor(p + tf.random_uniform(tf.shape(p), 0, 1))
-
-        def Gibbs(k):
-            def GibbsStep(count, k, xk):
-                hk = ProbSample(tf.sigmoid(tf.matmul(xk, weightMatrix) + hBias))
-                xk = ProbSample(tf.sigmoid(tf.matmul(hk, tf.transpose(weightMatrix)) + vBias))
-                return count + 1, k, xk
-
-            ct = tf.constant(0)
-            [_,_,x_sample] = control_flow_ops.while_loop(lambda count, num_iter, *args: count < num_iter, GibbsStep, [ct, tf.constant(k), x])
-
-            x_sample = tf.stop_gradient(x_sample)
-            return x_sample
-
-        x_sample = Gibbs(1)
-        h = ProbSample(tf.sigmoid(tf.matmul(x, weightMatrix) + hBias))
-        h_sample = ProbSample(tf.sigmoid(tf.matmul(x_sample, weightMatrix) + hBias))
-
-        size_bt = tf.cast(tf.shape(x)[0], tf.float32)
-        W_adder = tf.multiply(learningRate / size_bt,
-                              tf.subtract(tf.matmul(tf.transpose(x), h), tf.matmul(tf.transpose(x_sample), h_sample)))
-
-        bv_adder = tf.multiply(learningRate / size_bt,
-                               tf.reduce_sum(tf.subtract(x, x_sample), 0, True))
-        bh_adder = tf.multiply(learningRate / size_bt,
+        bv_adder = tf.multiply(self.learnRate / size_bt,
+                               tf.reduce_sum(tf.subtract(self.notedata, note_sample), 0, True))
+        bh_adder = tf.multiply(self.learnRate / size_bt,
                                tf.reduce_sum(tf.subtract(h, h_sample), 0, True))
 
-        trainUpdate = [weightMatrix.assign_add(W_adder), vBias.assign_add(bv_adder), hBias.assign_add(bh_adder)]
+        trainUpdate = [self.wMatrix.assign_add(W_adder), self.vBias.assign_add(bv_adder), self.hBias.assign_add(bh_adder)]
 
         with tf.Session() as session:
 
             init = tf.global_variables_initializer()
             session.run(init)
 
-            for epoch in tqdm(range(epochCount)):
+            for epoch in tqdm(range(self.epochs)):
 
                 for tData in self.trainDataset:
                     tData = num.array(tData)
-                    tData = tData[:int(num.floor(tData.shape[0] // timesteps) * timesteps)]
-                    tData = num.reshape(tData, [tData.shape[0] // timesteps, tData.shape[1] * timesteps])
+                    tData = tData[:int(num.floor(tData.shape[0] // self.timesteps) * self.timesteps)]
+                    tData = num.reshape(tData, [tData.shape[0] // self.timesteps, tData.shape[1] * self.timesteps])
 
-                    for i in range(1, len(tData), batchSize):
-                        tr_x = tData[i:i + batchSize]
-                        session.run(trainUpdate, feed_dict={x: tr_x})
+                    for i in range(1, len(tData), self.batchSize):
+                        tr_x = tData[i:i + self.batchSize]
+                        session.run(trainUpdate, feed_dict={self.notedata: tr_x})
 
 
-            sample = Gibbs(1).eval(session=session, feed_dict={x: num.zeros((5, vLayerNodes))})
+            sample = Gibbs(k=1, x=self.notedata, wMatrix=self.wMatrix, hBias=self.hBias, vBias=self.vBias).eval(session=session, feed_dict={self.notedata: num.zeros((5, self.vNodes))})
             for i in range(sample.shape[0]):
                 if not any(sample[i, :]):
                     continue
-                S = num.reshape(sample[i, :], (timesteps, 2 * notespan))
+                S = num.reshape(sample[i, :], (self.timesteps, 2 * self.notespan))
                 self.midi.FVtoMIDI(S, os.path.abspath(os.curdir) + "\\Test-Out-{}".format(i))
 
         return
 
     def Generate(self):
+        if not self.trained:
+            print("Can't generate on an untrained network!")
+            return
+
         return
 
